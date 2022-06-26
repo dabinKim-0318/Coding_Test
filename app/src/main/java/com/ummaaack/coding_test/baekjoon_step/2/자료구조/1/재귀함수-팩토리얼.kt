3a2941fd386fb472affd8d4ba3e224ed57846a35
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-06-26 22:21:58 +0900

    부캠

--- app/src/main/java/com/ummaaack/coding_test/baekjoon_step/2/자료구조/1/재귀함수-팩토리얼.kt
+++ app/src/main/java/com/ummaaack/coding_test/baekjoon_step/2/자료구조/1/재귀함수-팩토리얼.kt
@@ -1,5 +1,6 @@
 package com.ummaaack.coding_test.baekjoon_step
 
+import java.math.BigInteger
 import java.util.*
 
 fun main3() = with(Scanner(System.`in`)) {
@@ -65,7 +66,7 @@ fun main9(args: Array<String>) {
 
 //10진법 -> N진법
 //10부터는 A B C D E F G .. Z
-fun main() {
+fun mai3n() {
     val scan = Scanner(System.`in`)
     var n = scan.nextInt()
     val b = scan.nextInt()
@@ -116,3 +117,72 @@ internal class Graph(private val V: Int) {
         for (i in 0 until V) adj[i] = LinkedList()
     }
 }
+
+object Main {
+    var n = 0
+    var m = 0
+    var graph = Array(1000) { IntArray(1000) }
+
+    // DFS로 특정 노드를 방문하고 연결된 모든 노드들도 방문
+    fun dfs(x: Int, y: Int): Boolean {
+        // 주어진 범위를 벗어나는 경우에는 즉시 종료
+        if (x <= -1 || x >= n || y <= -1 || y >= m) {
+            return false
+        }
+        // 현재 노드를 아직 방문하지 않았다면
+        if (graph[x][y] == 0) {
+            // 해당 노드 방문 처리
+            graph[x][y] = 1
+            // 상, 하, 좌, 우의 위치들도 모두 재귀적으로 호출
+            dfs(x - 1, y)
+            dfs(x, y - 1)
+            dfs(x + 1, y)
+            dfs(x, y + 1)
+            return true
+        }
+        return false
+    }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val sc = Scanner(System.`in`)
+val list= mutableListOf(1,2,3)+ mutableListOf(3)
+        list.toMutableList().add(3)
+        // N, M을 공백을 기준으로 구분하여 입력 받기
+        n = sc.nextInt()
+        m = sc.nextInt()
+        sc.nextLine() // 버퍼 지우기
+
+        // 2차원 리스트의 맵 정보 입력 받기
+        for (i in 0 until n) {
+            val str = sc.nextLine()
+            for (j in 0 until m) {
+                graph[i][j] = str[j] - '0'
+            }
+        }
+        var d3=minOf(3,3)
+        var dd=BigInteger.ONE
+        // 모든 노드(위치)에 대하여 음료수 채우기
+        var result = 0
+        for (i in 0 until n) {
+            for (j in 0 until m) {
+                // 현재 위치에서 DFS 수행
+                if (dfs(i, j)) {
+                    result += 1
+                }
+            }
+        }
+        println(result) // 정답 출력
+    }
+}
+
+fun <T> permutation(el: List<T>, fin: List<T> = listOf(), sub: List<T> = el ): List<List<T>> {
+    return if(sub.isEmpty()) listOf(fin)
+    else sub.flatMap { permutation(el, fin + it, sub - it) }
+}
+
+fun main(args: Array<String>) {
+    val list = mutableListOf('a', 'b', 'c', 'd')
+    val list2 = permutation(list)
+    list2.forEach { print("$it ") }
+    println()}
