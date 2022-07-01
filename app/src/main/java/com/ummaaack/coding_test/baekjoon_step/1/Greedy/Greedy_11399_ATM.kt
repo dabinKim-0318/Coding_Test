ec75740c8284730706963c97e3497ca34dd684a9
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-07-01 17:29:48 +0900

    백준-구현

--- app/src/main/java/com/ummaaack/coding_test/baekjoon_step/1/Greedy/Greedy_11399_ATM.kt
+++ app/src/main/java/com/ummaaack/coding_test/baekjoon_step/1/Greedy/Greedy_11399_ATM.kt
@@ -3,15 +3,30 @@ package com.ummaaack.coding_test.baekjoon_step.`1`.Greedy
 import java.util.*
 
 fun main() = with(Scanner(System.`in`)) {
-    val people = nextInt()
-    val time = MutableList(people) { nextInt() }
-    var totalTime = 0
-
-    time.sort()
-
-    for (i in time.size downTo 1) {
-
-        totalTime += time[time.size - i] * i
+    //세 정수 A, B, C가 주어진다. 이때, 두 번째로 큰 정수를 출력하는 프로그램
+    var num = nextInt()
+    //6,9를 필터링한 리스트
+    var uniqueList = num.toString().toCharArray().filter { it != '6' && it != '9' }.toMutableList()
+    //uniqueList를 distinct한 리스트
+    var distinctList = uniqueList.distinct()
+    //중복 수 카운트 리스트
+    var countList = mutableListOf<Int>()
+    distinctList.forEach { i ->
+        countList.add(uniqueList.count { it == i })
     }
-    println(totalTime)
+    //카운트 리스트 중 가장 큰 수
+
+    var max = countList.maxOrNull() ?: 0
+
+    var sixNineNum = num.toString().toCharArray().count { it == '6' || it == '9' }
+    if(sixNineNum%2==0) sixNineNum/=2
+    else sixNineNum=sixNineNum/2 +1
+
+    if (max > sixNineNum) {
+        println(max)
+    } else if(sixNineNum==0){
+        println(1)
+    }else{
+        println(sixNineNum)
+    }
 }
