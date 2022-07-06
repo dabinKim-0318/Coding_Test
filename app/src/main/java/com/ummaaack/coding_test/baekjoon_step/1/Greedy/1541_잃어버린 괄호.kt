976235d11729713e55fcee5dc006029a62e2d78d
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-07-06 18:47:05 +0900

    구현

--- app/src/main/java/com/ummaaack/coding_test/baekjoon_step/1/Greedy/1541_잃어버린 괄호.kt
+++ app/src/main/java/com/ummaaack/coding_test/baekjoon_step/1/Greedy/1541_잃어버린 괄호.kt
@@ -1,3 +1,26 @@
 package com.ummaaack.coding_test.baekjoon_step
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
 
-//제일 작은 수에서 제일 큰수를 뺌
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    var n = readLine().toInt()
+    var list=mutableListOf<Pair<Int,Int>>()
+    var answer= MutableList(n){1}
+    for(item in 0 until n){
+        val speck=readLine()!!.split(" ").map{it.toInt()}
+        list.add(Pair(speck[0],speck[1]))
+    }
+    for(i in list.indices){
+        for(j in list.indices){
+            if(list[j].first>list[i].first && list[j].second>list[i].second){
+                answer[i]+=1
+            }
+        }
+    }
+    var sb=StringBuilder("")
+    for(item in answer){
+        sb.append(item.toString()+" ")
+    }
+    println(sb.toString())
+}
