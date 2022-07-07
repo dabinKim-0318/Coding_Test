a73a60c57103bdee6fbfb34be4463714475679a6
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-07-07 19:43:39 +0900

    bfs

--- app/src/main/java/com/ummaaack/coding_test/baekjoon_step/1/Greedy/1541_잃어버린 괄호.kt
+++ app/src/main/java/com/ummaaack/coding_test/baekjoon_step/1/Greedy/1541_잃어버린 괄호.kt
@@ -1,26 +1,53 @@
 package com.ummaaack.coding_test.baekjoon_step
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.util.*
 
-fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
-    var n = readLine().toInt()
-    var list=mutableListOf<Pair<Int,Int>>()
-    var answer= MutableList(n){1}
-    for(item in 0 until n){
-        val speck=readLine()!!.split(" ").map{it.toInt()}
-        list.add(Pair(speck[0],speck[1]))
-    }
-    for(i in list.indices){
-        for(j in list.indices){
-            if(list[j].first>list[i].first && list[j].second>list[i].second){
-                answer[i]+=1
-            }
+import java.util.*
+import kotlin.math.max
+
+val graph:Array<IntArray> = Array(250){IntArray(250){0}}
+var n=0
+var m=0
+var answer=0
+
+fun main(){
+    val nm=readLine()!!.split(" ").map{it.toInt()}
+    n=nm[0]
+    m=nm[1]
+
+    for(i in 0 until n){
+        val list=readLine()!!.split(" ").map{it.toInt()}
+        for(j in 0 until m){
+            graph[i][j]=list[j]
         }
     }
-    var sb=StringBuilder("")
-    for(item in answer){
-        sb.append(item.toString()+" ")
+
+    for(i in 0 until n){
+        for(j in 0 until m){
+            if(dfs(i,j)) answer++
+        }
     }
-    println(sb.toString())
+
+    println(answer)
+}
+
+fun dfs(x:Int,y:Int):Boolean{
+    if(x<0||x>=n||y<0||y>=m) return false
+
+    if(graph[x][y]==1){
+        graph[x][y]=0
+
+        dfs(x+1,y)
+        dfs(x-1,y)
+        dfs(x,y+1)
+        dfs(x,y-1)
+
+        dfs(x-1,y+1)
+        dfs(x+1,y+1)
+        dfs(x-1,y-1)
+        dfs(x+1,y-1)
+
+        return true
+    }
+
+    return false
+
 }
