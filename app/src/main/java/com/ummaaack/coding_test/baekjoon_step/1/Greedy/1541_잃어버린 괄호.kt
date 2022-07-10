a368656889cff8d7b1555be127c9141a89b4d05c
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-07-10 22:47:36 +0900

    정렬

--- app/src/main/java/com/ummaaack/coding_test/baekjoon_step/1/Greedy/1541_잃어버린 괄호.kt
+++ app/src/main/java/com/ummaaack/coding_test/baekjoon_step/1/Greedy/1541_잃어버린 괄호.kt
@@ -1,16 +1,2 @@
 package com.ummaaack.coding_test.baekjoon_step
 
-fun main(){
-    val num=readLine()!!
-    val list=num.toCharArray().map{
-        it.toString().toInt()
-    }.sortedDescending()
-
-    val sb=StringBuilder("")
-    list.forEach{
-        sb.append(it)
-    }
-
-    println(sb.toString().toInt())
-
-}
