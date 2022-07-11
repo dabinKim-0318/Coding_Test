4581bee6d5307cb7e8467bc985d1f0b17e8503ad
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-07-11 18:52:29 +0900

    [ADD] DP

--- app/src/main/java/com/ummaaack/coding_test/baekjoon_step/1/Greedy/1541_잃어버린 괄호.kt
+++ app/src/main/java/com/ummaaack/coding_test/baekjoon_step/1/Greedy/1541_잃어버린 괄호.kt
@@ -1,18 +1,42 @@
 package com.ummaaack.coding_test.baekjoon_step
 
+import java.util.*
 import kotlin.math.max
 
 fun main() {
 
     val n = readLine()!!.toInt()
-    val list = readLine()!!.split(" ").map { it.toInt() }
+    val list = IntArray(n + 1) { 0 }
     var d = MutableList(10000) { 0 }
-    d[0]=list[0]
-    for (i in 1..n - 1) {
+    for (i in 0 until n) {
+        list[i] = Scanner(System.`in`).nextInt()
+    }
+
+    for (i in 1..n) { //1 2 3 4 5
         //경우의 수중 가장 큰값
-        for (j in 1..i-1) {
-            d[i] = max(d[i],d[i-j]+list[j])
+        for (j in 1..i) { //1
+            d[i] = max(d[i], d[i - j] + list[j])
         }
     }
-    println(d[n - 1])
+    println(d[n])
+}
+//10 9 8 7 6
+
+
+object Beak11052 {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val sc = Scanner(System.`in`)
+        val n = sc.nextInt()
+        val arr = IntArray(n + 1)
+        val d = IntArray(n + 1)
+        for (i in 1..n) arr[i] = sc.nextInt()
+        for (i in 1..n) {
+            for (j in 1..i) {
+                d[i] = Math.max(d[i], d[i - j] + arr[j])
+            }
+        }
+        println(d[n])
+        sc.close()
+    }
 }
