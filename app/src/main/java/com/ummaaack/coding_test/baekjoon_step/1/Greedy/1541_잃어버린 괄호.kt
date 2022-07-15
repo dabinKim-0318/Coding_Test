9829a0ced4fb5278eab672c561229117f366f6e8
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-07-15 23:25:48 +0900

    연구소

--- app/src/main/java/com/ummaaack/coding_test/baekjoon_step/1/Greedy/1541_잃어버린 괄호.kt
+++ app/src/main/java/com/ummaaack/coding_test/baekjoon_step/1/Greedy/1541_잃어버린 괄호.kt
@@ -1,59 +1,28 @@
 package com.ummaaack.coding_test.baekjoon_step
 
+import java.io.BufferedReader
+import java.io.InputStreamReader
 import java.util.*
 import kotlin.math.max
 
-val graph: Array<IntArray> = Array(1000) { IntArray(1000) { 0 } }
-var isVisited: Array<BooleanArray> = Array(1000) { BooleanArray(1000) { false } }
-var virus: Queue<Pair<Int, Int>> = LinkedList()
-var n = 0
-var m = 0
-val dy = arrayOf(-1, 0, 1, 0)
-val dx = arrayOf(0, 1, 0, -1)
-var max = 0
-var answer = Integer.MIN_VALUE
-
 fun main() {
-    val nm = readLine()!!.split(" ").map { it.toInt() }
-    n = nm[0]
-    m = nm[1]
-    max = n * m
-
+    val n = readLine()!!.toInt()
+    val list = mutableListOf<Int>()
+    val st = Stack<Int>()
+    val answerSt = Stack<Int>()
+    val charSt = mutableListOf<Char>()
     for (i in 0 until n) {
-        val list = readLine()!!.split(" ").map { it.toInt() }
-        for (j in 0 until m) {
-            graph[i][j] = list[j]
-            if (graph[i][j] == 0) continue
-            max--
-            if (graph[i][j] == 2)
-                virus.offer(Pair(i, j))
+        list.add(readLine()!!.toInt())
+    }
+    var index = 0
+    for (i in 1..n) {
+        st.push(i)
+        charSt.add('+')
+        while (st.isNotEmpty() && st.peek() == list[index]) {
+            answerSt.push(st.pop())
+            charSt.add('-')
+            index++
         }
     }
-    permutation(0)
-}
 
-val copyList = graph.copyOf()
-val isVisitedPer = Array(n){BooleanArray(m){false} }
-fun permutation(dept: Int) {
-
-    if (dept == 3) {
-        var countVirusRange=0
-        val q:Queue<Pair> = LinkedList
-
-    }
-
-    for (x in 0 until n) {
-        for (y in 0 until m) {
-            if (isVisitedPer[x][y]==false&&copyList[x][y] == 0) {
-                graph[x][y] = 1
-                isVisitedPer[x][y]=true
-                max--
-                permutation(dept + 1)
-
-                isVisited[x][y]=false
-                graph[x][y]=0
-                max++
-            }
-        }
-    }
 }
