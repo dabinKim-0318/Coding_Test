72ef9882cd5cece1206491b9b4e1a4d6fa16985f
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-07-18 18:42:49 +0900

    문자열뒤집기2

--- app/src/main/java/com/ummaaack/coding_test/baekjoon_step/1/Greedy/1541_잃어버린 괄호.kt
+++ app/src/main/java/com/ummaaack/coding_test/baekjoon_step/1/Greedy/1541_잃어버린 괄호.kt
@@ -1,28 +1,32 @@
 package com.ummaaack.coding_test.baekjoon_step
 
+
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.util.*
-import kotlin.math.max
 
-fun main() {
-    val n = readLine()!!.toInt()
-    val list = mutableListOf<Int>()
-    val st = Stack<Int>()
-    val answerSt = Stack<Int>()
-    val charSt = mutableListOf<Char>()
-    for (i in 0 until n) {
-        list.add(readLine()!!.toInt())
-    }
-    var index = 0
-    for (i in 1..n) {
-        st.push(i)
-        charSt.add('+')
-        while (st.isNotEmpty() && st.peek() == list[index]) {
-            answerSt.push(st.pop())
-            charSt.add('-')
-            index++
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val input = readLine()
+    val stack = Stack<Char>()
+    var flag = true //true면 뒤집음
+    val sb = StringBuilder("")
+    for (i in input.indices) {
+        val char = input[i]
+        if (char == '<') flag = false
+        if (char == '>') {
+            flag = true
+            sb.append(char)
+            continue
+        }
+
+        if (flag) {
+            if (char != ' ') stack.push(char)
+        }
+        if (!flag || char == ' ') {
+          while(stack.isNotEmpty()) sb.append(stack.pop())
+            sb.append(char)
         }
     }
-
+    while(stack.isNotEmpty()) sb.append(stack.pop())
+    println(sb.toString())
 }
