d0193f44fa721fd76af9587d76d1cdd6055ce026
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-07-21 12:59:52 +0900

    골드바흐 추측

--- app/src/main/java/com/ummaaack/coding_test/baekjoon_step/1/Greedy/1541_잃어버린 괄호.kt
+++ app/src/main/java/com/ummaaack/coding_test/baekjoon_step/1/Greedy/1541_잃어버린 괄호.kt
@@ -1,26 +1,77 @@
 package com.ummaaack.coding_test.baekjoon_step
 
-
 import java.io.BufferedReader
 import java.io.InputStreamReader
-import kotlin.math.abs
 
-var ans = 1L
-fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
-    //2 4 8  ->차이의 최대공약수
-    val (n, s) = readLine().split(" ").map { it.toLong() } //수 , 내좌표
-    val list = readLine().split(" ").map { it.toLong() } //동생 좌표
-    val cha = mutableListOf<Long>() //차이 리스트
-    list.forEach {
-        cha.add(abs(it - s))
-    }
-    ans = cha.first()
-    for (i in cha.indices) {
-        if (i == cha.size - 1) break
-        ans = gcd(ans, cha[i + 1])
-    }
 
-    println(ans)
+var sosuList = mutableListOf<Boolean>()
+var list = mutableListOf<Int>()
+var pickedNum = mutableListOf<Int>()
+var ans = 0 //가능한 경우수 카운트
+var n = 0
+fun mai4n() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    var input = readLine().toInt()
+    for (i in 1..input) {
+        n = readLine().toInt()
+        getSosu(n)
+
+        jo(0, 2, 0, sosuList)
+        sosuList.forEachIndexed { i, it ->
+            if (it && i * 2 == n) ans += 1
+        }
+        println(ans)
+        ans = 0
+        list.clear()
+    }
 }
 
-fun gcd(a: Long, b: Long):Long = if (b != 0L) gcd(b, a % b) else a
+fun getSosu(n: Int) {
+    sosuList = MutableList(n + 1) { true }
+    sosuList[0] = false
+    sosuList[1] = false
+    for (i in 2 until n + 1) {
+        if (sosuList[i]) {
+            for (j in 2 * i until n + 1 step i) {
+                sosuList[j] = false
+            }
+        }
+    }
+}
+
+fun jo(cnt: Int, depth: Int, beginWith: Int, list: List<Boolean>) {
+    if (cnt == depth) {
+        //  println(pickedNum)
+        if (pickedNum.sum() == n) ans += 1
+        return
+    }
+
+    for (i in beginWith until list.size) {
+        if (list[i]) {
+            pickedNum.add(i)
+            jo(cnt + 1, depth, i + 1, list)
+            pickedNum.removeLast()
+        }
+    }
+}
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val bol = BooleanArray(100001) { true }
+    bol[0] = false
+    bol[1] = false
+    for (i in 2 until bol.size) {
+        if (bol[i]) {
+            for (j in 2 * i until bol.size step i) {
+                bol[j] = false
+            }
+        }
+    }
+    val input = readLine().toInt()
+    for (i in 1..input){
+        val n=readLine().toInt()
+        var ans=0
+        for(j in 2..n/2){
+
+        }
+    }
+}
+
