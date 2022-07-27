07e73df15c59721177b67f6315198d94bb7ed4eb
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-07-27 19:28:47 +0900

    브루트 포스 - 재귀

--- app/src/main/java/com/ummaaack/coding_test/baekjoon_step/1/Greedy/1541_잃어버린 괄호.kt
+++ app/src/main/java/com/ummaaack/coding_test/baekjoon_step/1/Greedy/1541_잃어버린 괄호.kt
@@ -2,90 +2,43 @@ package com.ummaaack.coding_test.baekjoon_step
 
 import java.io.BufferedReader
 import java.io.InputStreamReader
-import kotlin.math.max
+import java.lang.StringBuilder
+import java.util.*
 
-
-val graph: Array<CharArray> = Array(100) { CharArray(100) { ' ' } }
-val bool: Array<BooleanArray> = Array(100) { BooleanArray(100) { false } }
-var n = 0
-var m = 0
-val xx = listOf(-1, 1, 0, 0)
-val yy = listOf(0, 0, -1, 1)
-var ans = 0
+var list = mutableListOf<Char>()
+var pickedNum = mutableListOf<Char>()
+var bolList = BooleanArray(list.size) { false }
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
-    val input = readLine().toInt()
-    n = input
-    m = input
-
-    for (i in 0 until n) {
-        val str = readLine()
-        for (j in 0 until m) {
-            graph[i][j] = str[j]
-        }
+    val (l, c) = readLine().split(" ").map { it.toInt() }
+    val input = StringTokenizer(readLine()) //a c i s t w
+    for (i in 0..c - 1) {
+        list.add(input.nextToken()[0])
     }
-    ans = max(ans, checkCanIt())
-    for (i in 0 until n) {
-        for (j in 0 until m) {
-            swith(i, j)
-        }
-    }
-    println(ans)
+    list.sort()
+    bolList = BooleanArray(list.size) { false }
+    sun(0, l, 0)
 }
 
-fun swith(x: Int, y: Int) {
-    for (i in xx.indices) {
-        var temp = graph[x][y]
-        if (x < 0 || x >= n || y >= m || y < 0) return
-        graph[x][y] = graph[x + i][y]
-        graph[x + i][y] = temp
-        ans = max(ans, checkCanIt())
-        temp = graph[x][y]
-        graph[x][y] = graph[x + i][y]
-        graph[x + i][y] = temp
-    }
-    for (i in yy.indices) {
-        var temp = graph[x][y]
-        if (x < 0 || x >= n || y >= m || y < 0) return
-        graph[x][y] = graph[x][y + i]
-        graph[x][y + i] = temp
-        ans = max(ans, checkCanIt())
-        temp = graph[x][y]
-        graph[x][y] = graph[x][y + i]
-        graph[x][y + i] = temp
-    }
+fun sun(cnt: Int, depth: Int, beginWith: Int) {
+    if (cnt == depth) {
+        val count = pickedNum.count { it == 'a' || it == 'e' || it == 'i' || it == 'o' || it == 'u' }
+        val count2 = depth - count
 
-}
-
-fun checkCanIt(): Int {
-    var maxCnt = 0
-    for (i in 0 until n) {
-        var curRowCnt = 1
-        var lastChar = graph[i][0]
-        for (j in 1 until n) {
-            var curChar = graph[i][j]
-            if (curChar == lastChar) curRowCnt++
-            else {
-                lastChar = curChar
-                if (maxCnt < curRowCnt) maxCnt = curRowCnt
-                curRowCnt = 1
+        if (count > 0 && count2 >= 2) {
+            var sb = StringBuilder("")
+            pickedNum.forEach {
+                sb.append(it)
             }
-        }
-        if (maxCnt < curRowCnt) maxCnt = curRowCnt
-    }
-    for (i in 0 until n) {
-        var curColCnt = 1
-        var lastChar = graph[0][i]
-        for (j in 1 until n) {
-            var curChar = graph[j][i]
-            if (curChar == lastChar) curColCnt++
-            else {
-                lastChar = curChar
-                if (maxCnt < curColCnt) maxCnt = curColCnt
-                curColCnt = 1
-            }
-        }
-        if (maxCnt < curColCnt) maxCnt = curColCnt
-    }
+            print(sb.append("\n"))
 
-    return maxCnt
+        }
+        return
+    }
+    for (i in beginWith until list.size) {
+        pickedNum.add(list[i])
+        sun(cnt + 1, depth, i + 1)
+        pickedNum.removeLast()
+    }
 }
+
+
