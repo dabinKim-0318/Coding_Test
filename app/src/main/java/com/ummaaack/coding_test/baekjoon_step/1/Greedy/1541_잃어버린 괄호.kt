9228165b78e1a73dd616ffbfc8a1a5ea2873a1f9
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-07-28 16:23:24 +0900

    부등호

--- app/src/main/java/com/ummaaack/coding_test/baekjoon_step/1/Greedy/1541_잃어버린 괄호.kt
+++ app/src/main/java/com/ummaaack/coding_test/baekjoon_step/1/Greedy/1541_잃어버린 괄호.kt
@@ -4,41 +4,70 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.lang.StringBuilder
 import java.util.*
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
 
-var list = mutableListOf<Char>()
-var pickedNum = mutableListOf<Char>()
+val list = MutableList(10) { it } //0..9
 var bolList = BooleanArray(list.size) { false }
+var pickedNum = mutableListOf<Int>()
+var buList = mutableListOf<String>()
+var ans = mutableListOf<Long>()
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
-    val (l, c) = readLine().split(" ").map { it.toInt() }
-    val input = StringTokenizer(readLine()) //a c i s t w
-    for (i in 0..c - 1) {
-        list.add(input.nextToken()[0])
+    //부등호 개수 k
+    //넣을 수 있는 수 리스트: 0부터 9
+    //순서열 ->k+1개의 사이즈 ->순열
+    //만족하는 순열 add하고 최대 최소따로 출력
+    val k = readLine().toInt() //9개의 부등호
+    val st = StringTokenizer(readLine())
+    for (i in 1..k) {
+        buList.add(st.nextToken())
     }
-    list.sort()
-    bolList = BooleanArray(list.size) { false }
-    sun(0, l, 0)
+    sun(0, k + 1)
+    ans.sort()
+    println(ans.last())
+    if (ans.first().toString().length == k) println("0"+ans.first())
+    else println(ans.first())
 }
 
-fun sun(cnt: Int, depth: Int, beginWith: Int) {
+fun sun(cnt: Int, depth: Int) {
     if (cnt == depth) {
-        val count = pickedNum.count { it == 'a' || it == 'e' || it == 'i' || it == 'o' || it == 'u' }
-        val count2 = depth - count
-
-        if (count > 0 && count2 >= 2) {
+        if (check(pickedNum)) {
             var sb = StringBuilder("")
             pickedNum.forEach {
-                sb.append(it)
+                sb.append(it.toString())
             }
-            print(sb.append("\n"))
-
+            ans.add(sb.toString().toLong())
         }
         return
     }
-    for (i in beginWith until list.size) {
-        pickedNum.add(list[i])
-        sun(cnt + 1, depth, i + 1)
-        pickedNum.removeLast()
+
+    for (i in 0 until list.size) {
+        if (!bolList[i]) {
+            bolList[i] = true
+            pickedNum.add(list[i])
+            sun(cnt + 1, depth)
+            bolList[i] = false
+            pickedNum.removeLast()
+        }
     }
 }
 
-
+//5 6 8 9 0 2 3 4 5 1 ->10개
+fun check(list: MutableList<Int>): Boolean {
+    var flag = true
+    for (i in 0 until buList.size) { //0..9
+        if (buList[i] == "<") {
+            if (list[i] > list[i + 1]) {
+                flag = false
+                break
+            }
+        } else {
+            if (list[i] < list[i + 1]) {
+                flag = false
+                break
+            }
+        }
+    }
+    return flag
+}
