8ac6893906ccbe20010e9c0a24ac41c6b89adc87
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-08-02 19:55:41 +0900

    오큰수

--- app/src/main/java/com/ummaaack/coding_test/baekjoon_step/1/Greedy/1541_잃어버린 괄호.kt
+++ app/src/main/java/com/ummaaack/coding_test/baekjoon_step/1/Greedy/1541_잃어버린 괄호.kt
@@ -5,65 +5,24 @@ import java.io.InputStreamReader
 import java.util.*
 
 
-val list = mutableListOf<Move>()
-val ans = mutableListOf<Int>()
-var kk = 0
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
-    print(bfs().toString() + " ")
-
-    var target = kk
-    while (target != -1) {
-        for (it in list) {
-        //    println(list)
-            if (it.now == target) {
-                println(it.now)
-                ans.add(target)
-                target = it.before
-            }
+    val n = readLine().toInt()
+    val stack = Stack<Int>()
+    val st = StringTokenizer(readLine())
+    val list = mutableListOf<Int>()
+    repeat(n){
+        list.add(st.nextToken().toInt())
+    }
+    for(i in 0 until list.size){
+        while(stack.isNotEmpty()&&list[stack.peek()]<list[i]){
+            list[stack.pop()]=list[i]
         }
+        stack.push(i)
     }
-
-    ans.reversed().forEach {
-        print("${it}")
+    while(stack.isNotEmpty()){
+        list[stack.pop()]=-1
     }
+    var sb=StringBuilder("")
+    list.forEach {sb.append("$it ")}
+    println(sb.toString())
 }
-
-private fun bfs(): Int {
-    val (n, k) = readLine()!!.split(" ").map { it.toInt() }
-    kk = k
-    val queue: Queue<Move> = LinkedList()
-    val visited = BooleanArray(100_001) { false }
-
-    queue.add(Move(n, 0, -1))
-    visited[n] = true
-    list.add(Move(n, 0, -1)) //Move(5,0,-1)
-    while (queue.isNotEmpty()) {
-        val current = queue.poll() //Move
-        if (current.now == k) return current.time
-
-        if (checkRange(current.now - 1) && !visited[current.now - 1]) {
-            queue.add(Move(current.now - 1, current.time + 1, current.now))
-            list.add(Move(current.now - 1, current.time + 1, current.now))
-            visited[current.now - 1] = true
-        }
-        if (checkRange(current.now + 1) && !visited[current.now + 1]) {
-            queue.add(Move(current.now + 1, current.time + 1, current.now))
-            list.add(Move(current.now - 1, current.time + 1, current.now))
-            visited[current.now + 1] = true
-        }
-        if (checkRange(current.now * 2) && !visited[current.now * 2]) {
-            queue.add(Move(current.now * 2, current.time + 1, current.now))
-            list.add(Move(current.now * 2, current.time + 1, current.now))
-            visited[current.now * 2] = true
-        }
-    }
-    return -1
-}
-
-fun checkRange(x: Int) = x in 0..100_000
-
-data class Move(
-    val now: Int,
-    val time: Int,
-    val before: Int
-)
