519fdc48ba41c78765a858f5dff8300fd2af5dec
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-08-03 21:32:17 +0900

    수열 - 1 2 3

--- app/src/main/java/com/ummaaack/coding_test/baekjoon_step/1/Greedy/1541_잃어버린 괄호.kt
+++ app/src/main/java/com/ummaaack/coding_test/baekjoon_step/1/Greedy/1541_잃어버린 괄호.kt
@@ -4,25 +4,43 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.util.*
 
-
+var visited = booleanArrayOf()
+var list = intArrayOf()
+var pickedNum = mutableListOf<Int>()
+var ansList = mutableListOf<MutableList<Int>>()
+var target = mutableListOf<Int>()
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
-    val n = readLine().toInt()
-    val stack = Stack<Int>()
-    val st = StringTokenizer(readLine())
-    val list = mutableListOf<Int>()
-    repeat(n){
-        list.add(st.nextToken().toInt())
+    val n = readLine().toInt() //4
+    target = readLine().split(" ").map { it.toInt() }.toMutableList()
+    list = IntArray(n) { it + 1 }
+    visited = BooleanArray(n) { false }
+    if (target == list.toMutableList()) println(-1)
+    else {
+        if (nextPermutation(list)) list.forEach { print(it) }
     }
-    for(i in 0 until list.size){
-        while(stack.isNotEmpty()&&list[stack.peek()]<list[i]){
-            list[stack.pop()]=list[i]
-        }
-        stack.push(i)
+}
+
+private fun nextPermutation(arr: IntArray): Boolean {
+    var i = arr.size - 1
+    while (i > 0 && arr[i - 1] <= arr[i]) i--
+    if (i <= 0) return false
+
+    var j = arr.size - 1
+
+    while (arr[j] >= arr[i - 1]) j--
+
+    swap(arr, i - 1, j)
+    j = arr.size - 1
+    while (i < j) {
+        swap(arr, i, j)
+        i++
+        j--
     }
-    while(stack.isNotEmpty()){
-        list[stack.pop()]=-1
-    }
-    var sb=StringBuilder("")
-    list.forEach {sb.append("$it ")}
-    println(sb.toString())
+    return true
+}
+
+private fun swap(arr: IntArray, index1: Int, index2: Int) {
+    val temp = arr[index1]
+    arr[index1] = arr[index2]
+    arr[index2] = temp
 }
