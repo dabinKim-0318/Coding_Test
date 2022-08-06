fc38e50fd4138f92bace92c06645f273057db006
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-08-06 22:01:47 +0900

    DFS

--- app/src/main/java/com/ummaaack/coding_test/baekjoon_step/1/Greedy/1541_잃어버린 괄호.kt
+++ app/src/main/java/com/ummaaack/coding_test/baekjoon_step/1/Greedy/1541_잃어버린 괄호.kt
@@ -47,55 +47,52 @@ private fun dfs(n: Int) {
 }*/
 
 
-private const val m = 0
-private lateinit var list: Array<ArrayList<Int>>
-private const val ans = 0
-private lateinit var v: BooleanArray
+val queue: Queue<Char> = LinkedList<Char>()
+var target = ""
+var list = ""
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val str = readLine()
+    target = readLine()
 
-
-fun main(args: Array<String>) {
-    val br = BufferedReader(InputStreamReader(System.`in`))
-    val bw = BufferedWriter(OutputStreamWriter(System.out))
-    var st = StringTokenizer(br.readLine())
-    val N = st.nextToken().toInt()
-    val M = st.nextToken().toInt()
-    Main.m = M
-
-    //DFS를 위한 인접리스트 구현하기
-    list = Array<ArrayList<Int>>(N) { ArrayList(N) { 0 } }
-    v = BooleanArray(N)
-    for (i in 0 until N) {
-        list.get(i) = ArrayList<Int>()
-    }
-    for (i in 0 until M) {
-        st = StringTokenizer(br.readLine())
-        val n1 = st.nextToken().toInt()
-        val n2 = st.nextToken().toInt()
-        Main.list.get(n1).add(n2)
-        Main.list.get(n2).add(n1)
-    }
-
-    //N-1까지의 모든 정점에서 DFS를 통해 확인
-    for (i in 0 until N) {
-        if (Main.ans == 0) Main.dfs(i, 1)
-    }
-    bw.write(Integer.toString(Main.ans))
-    bw.flush()
-    bw.close()
-    br.close()
-}
-
-fun dfs(start: Int, depth: Int) {
-    //System.out.println(start + " " + depth); //방문 정점과 깊이를 확인해보고 싶을 때 사용
-    if (depth == 5) {
-        Main.ans = 1
-        return
-    }
-    Main.v.get(start) = true
-    for (i in Main.list.get(start)) {
-        if (!Main.v.get(i)) {
-            Main.dfs(i, depth + 1)
+    for (i in 0 until str.length) {
+        if (target.contains(str[i])
+        ) {
+            list += str[i]
+            println(list)
+            println(i)
+        } else {
+            println(i)
+            check()
+            queue.add(str[i])
+            println(queue)
         }
     }
-    Main.v.get(start) = false
+    var sb = StringBuilder()
+    queue.forEach {
+        sb.append(it)
+    }
+    println(sb)
+}
+
+private fun check() {
+    while (true) {
+        if (list.isEmpty()) break
+        if (list.length >= target.length) { //ca4ca4
+            for (i in 0 until list.length) {
+                if (i + target.length - 1 >= list.length - 1) break
+                if (list.substring(i..i + target.length - 1) == target) {
+
+                    list = (list.removeRange(i..i + target.length - 1))
+                    break
+                }
+            }
+        }
+    }
+    if (list.isNotEmpty()) {
+        println(list)
+        list.forEach {
+            queue.add(it)
+            println(queue)
+        }
+    }
 }
